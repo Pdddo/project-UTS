e253be4b4bff01d31d8301f3cec58942a544ecce
rainnfu <87825532+rainnfu@users.noreply.github.com>
2025-04-28 13:50:30 +0800

    Update Garden.java

--- Garden.java
+++ Garden.java
@@ -1,17 +1,1 @@
-import java.util.ArrayList;
-import java.util.List;
 
-public class Garden{
-    private List<Plant> plants;
-    private String layoutName;
-
-    public Garden(String layoutName) {
-        this.layoutName = layoutName;
-        this.plants = new ArrayList<>();
-    }
-
-    public void addPlant(Plant plant) {
-        plants.add(plant);
-        
-    }
-}
