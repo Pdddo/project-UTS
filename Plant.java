0edf63d488dd916143fab0dafbe923da35c166a5
nabilasalsabilaaa <naabilasalsabiilaa@gmail.com>
2025-05-08 14:51:37 +0800

    perbaikan plant class

--- Plant.java
+++ Plant.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
-
-public class Plant {
-    Scanner sc = new Scanner(System.in);
+public abstract class Plant {
     protected String name;
     protected String scientificName;
     protected int age;
@@ -22,89 +19,34 @@ public class Plant {
     }
 
     public void grow(int type) {
-        do {
-            System.out.println("Pilih opsi untuk tambahan nutrisi :");
-            System.out.println("1. Pupuk");
-            System.out.println("2. Vitamin");
-            System.out.println("> ");
-
-            while (!sc.hasNextInt()) {
-                System.out.println("Input harus angka");
-                sc.next();
-            }
-
-            type = sc.nextInt();
-            if (type == 1) {
-                this.nutrition += 10;
-                System.out.println("Pupuk telah ditambahkan. Nutrisi bertambah 10 exp");
-            } else if (type == 2) {
-                this.nutrition += 20;
-                System.out.println("Vitamin telah ditambahkan. Nutrisi bertambah 20 exp");
-            } else {
-                System.out.println("Pilih angka antara 1 atau 2");
-            }
-        } while (type != 1 && type != 2);
-        
+        if (type == 1) {
+            this.nutrition += 10;
+        } else if (type == 2) {
+            this.nutrition += 20;
+        }
         updateStatus();
     }
 
-    public void water(int choice) {
-        do {
-            System.out.println("Apakah ingin menyiram tanaman?");
-            System.out.println("1. Ya");
-            System.out.println("2. Tidak");
-            System.out.println("> ");
-        
-            while (!sc.hasNextInt()) {
-                System.out.println("Input harus angka");
-                sc.next();
-            }
+    public void water(boolean isWatered) {
 
-            choice = sc.nextInt();
-
-            if (choice == 1) {
-                this.hydration += 5;
-                System.out.println("Tanaman berhasil disiram. Nutrisi bertambah 5 exp");
-            } else if (choice == 2) {
-                this.hydration += 0;
-                System.out.println("Tanaman tidak disiram");
-            } else {
-                System.out.println("Pilih angka antara 1 atau 2");
-            }
-        } while (choice != 1 && choice != 2);
-
+        if (isWatered) {
+            this.hydration += 5;
+        }
         updateStatus();
     }
 
-    public void weather(int todayWeather) {
-        do {
-            System.out.println("Bagaimana cuaca hari ini?");
-            System.out.println("1. Cerah");
-            System.out.println("2. Mendung");
-            System.out.println("3. Hujan");
-            System.out.println("> ");
-
-            while (!sc.hasNextInt()) {
-                System.out.println("Input harus angka");
-                sc.next();
-            }
-
-            todayWeather = sc.nextInt();
-
-            if (todayWeather == 1) {
-                this.sunlight += 5;
-                System.out.println("Hari ini cerah! Nutrisi tanaman mu bertambah 5 exp");
-            } else if (todayWeather == 2) {
-                this.sunlight += 2;
-                System.out.println("Waduh lagi mendung nih. Tanaman  mu bertambah 2 exp");
-            } else if (todayWeather == 3) {
-                this.sunlight += 0;
-                System.out.println("YAHHH HUJAN. Tanaman mu kekurangan cahaya matahari");
-            } else {
-                System.out.println("Pilih angka antara 1, 2 dan 3");
-            }
-        } while (todayWeather < 1 || todayWeather > 3);
-
+    public void weather(int weatherType) {
+        switch (weatherType) {
+            case 1:
+                sunlight += 5;
+                break; //cerah
+            case 2:
+                sunlight += 2;
+                break; //mendung
+            case 3:
+                sunlight += 0;
+                break; //hujan
+        }
         updateStatus();
     }
     
@@ -131,21 +73,20 @@ public class Plant {
     
 }
 
+// class decorativePlants extends Plant {
+//     public decorativePlants(String name, String scientificName, int age) {
+//         super(name, scientificName, age);
+//     }
+// }
 
-class decorativePlants extends Plant {
-    public decorativePlants(String name, String scientificName, int age) {
-        super(name, scientificName, age);
-    }
-}
+// class medicinalPlants extends Plant {
+//     public medicinalPlants(String name, String scientificName, int age) {
+//         super(name, scientificName, age);
+//     }
+// }
 
-class medicinalPlants extends Plant {
-    public medicinalPlants(String name, String scientificName, int age) {
-        super(name, scientificName, age);
-    }
-}
-
-class  vegetablePlants extends Plant {
-    public vegetablePlants(String name, String scientificName, int age) {
-        super(name, scientificName, age);
-    }
-}
+// class  vegetablePlants extends Plant {
+//     public vegetablePlants(String name, String scientificName, int age) {
+//         super(name, scientificName, age);
+//     }
+// }
